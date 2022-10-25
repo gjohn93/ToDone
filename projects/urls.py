8ea8cced1ec894a112c377425dda6006d1a026a2
Gina <gejohn793@gmail.com>
2022-10-25 13:35:12 -0500

    Finished Feature 13

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from projects.views import project_list
+from projects.views import project_list, detail_view
 
 urlpatterns = [
     path("", project_list, name= "list_projects"),
+    path("<int:id>/", detail_view , name="show_project")
 ]
--- projects/views.py
+++ projects/views.py
@@ -13,5 +13,11 @@ def project_list(request):
 
     return render(request, "projects/list.html", context)
 
-
+@login_required
+def detail_view(request, id):
+    project_details = Project.objects.get(id=id)
+    context = {
+        "project_details": project_details
+    }
+    return render(request, "projects/detail.html", context)
 # Create your views here.
