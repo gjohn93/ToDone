580ed3f354de3e754aecadc331ae7ea09d9741ba
Gina <gejohn793@gmail.com>
2022-10-28 09:56:28 -0500

    Final submission with all tests passing

--- tracker/urls.py
+++ tracker/urls.py
@@ -17,14 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
+
 def redirect_to_project_list(request):
     return redirect("list_projects")
 
 
 urlpatterns = [
-    path("", redirect_to_project_list, name = "home"),
+    path("", redirect_to_project_list, name="home"),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("accounts/", include("accounts.urls")),
-    path("tasks/", include("tasks.urls"))
+    path("tasks/", include("tasks.urls")),
 ]
